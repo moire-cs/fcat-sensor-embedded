42664baa4bfd121635de0c4edc66a7d37c41cc66
MadBeignet <maddie.wisinski@ymail.com>
2024-03-16 16:29:53 -0500

    basic gateway functionality

--- Mesh/src/main-gateway.cpp
+++ Mesh/src/main-gateway.cpp
@@ -1,10 +1,99 @@
+#include <RHMesh.h>
+#include <RH_RF95.h>
 #include <SPI.h>
-#include <Arduino.h>
+#include <esp_task_wdt.h>
+#include <Wire.h>
+#include <cstring>
+#include "driver/gpio.h"
+#include "radio_pinouts_and_constants.h"
+#include "meshing.h"
+#include ".env.h"
+#include <WiFi.h>
+#include "time.h"
 
+// Variable to save current epoch time
+unsigned long epochTime;
+
+void rhSetup();
+void runGatewayReceiver(uint16_t wait_time, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_);
+void runGatewaySender(String *packetInfo, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_);
+
+time_t start;
+time_t end;
 void setup()
 {
+    Serial.begin(115200);
+
+    esp_task_wdt_init(WDT_TIMEOUT, true); // enable panic so ESP32 restarts
+    esp_task_wdt_add(NULL);
+
+    rhSetup();
+    Serial.println(" ---------------- GATEWAY " + String(selfAddress_) +
+                   " INIT ---------------- ");
+}
+
+uint8_t _msgRcvBuf[RH_MESH_MAX_MESSAGE_LEN];
+
+// Get the current time
+unsigned long getTime()
+{
+    time_t now;
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo))
+    {
+        // Serial.println("Failed to obtain time");
+        return (0);
+    }
+    time(&now);
+    return now;
+}
+
+// Initialize WiFi (Gateway will use it to send data to backend)
+void initWiFi()
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print('.');
+        delay(1000);
+    }
+    Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
+    gettimeofday(&start, NULL);
+    epochTime = getTime();
+
+    // GET route to receive information from backend (cycle period, num measurements, etc.)
+    unsigned long gatewaySleep = 24; // hours
+
+    cycle();
+
+    gettimeofday(&end, NULL);
+    unsigned long time_taken = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
+    esp_err_t sleep_error = esp_sleep_enable_timer_wakeup(gatewaySleep * hours_to_seconds * microseconds - time_taken); // takes into account time between start and sleep
+    esp_deep_sleep_start();
+}
+
+void cycle()
+{
+
+    uint8_t _msgFrom;
+    uint8_t _msgRcvBufLen = sizeof(_msgRcvBuf);
+
+    if (mode_ == SENDING_MODE)
+    {
+        String packetInfo = "Hello"; // temp message
+        Serial.printf("Sending data to %d...", targetAddress_);
+        runGatewaySender(&packetInfo, _msgRcvBuf, &_msgRcvBufLen, &_msgFrom, RFM95Modem_, RHMeshManager_);
+
+        mode_ = RECEIVING_MODE;
+    }
+    if (mode_ == RECEIVING_MODE)
+    {
+        runGatewayReceiver(120 * 1000, _msgRcvBuf, &_msgRcvBufLen, &_msgFrom, RFM95Modem_, RHMeshManager_);
+    }
 }
--- Mesh/src/meshing.h
+++ Mesh/src/meshing.h
@@ -1,7 +1,6 @@
 void runReceiver(uint16_t wait_time, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_)
 {
     // while at it, wait for a message from other nodes
-    Serial.println("Receiving mode active");
 
     if (RHMeshManager_.recvfromAckTimeout(_msgRcvBuf, _msgRcvBufLen, wait_time, _msgFrom))
     {
@@ -29,7 +28,7 @@ void runReceiver(uint16_t wait_time, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen
     }
 }
 
-void runSending(String *packetInfo, uint8_t targetAddress_, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_)
+void runSender(String *packetInfo, uint8_t targetAddress_, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_)
 {
     uint8_t _err =
         RHMeshManager_.sendtoWait(reinterpret_cast<uint8_t *>(packetInfo),
@@ -41,6 +40,82 @@ void runSending(String *packetInfo, uint8_t targetAddress_, uint8_t *_msgRcvBuf,
         esp_task_wdt_reset();
         Serial.printf(" successfull! Awaiting for Reply\n");
 
+        if (RHMeshManager_.recvfromAckTimeout(_msgRcvBuf, _msgRcvBufLen, 3000, _msgFrom))
+        {
+            char buf_[RH_MESH_MAX_MESSAGE_LEN];
+
+            std::sprintf(buf_, "%s", reinterpret_cast<char *>(_msgRcvBuf));
+            msgRcv = std::string(buf_);
+            Serial.printf("[%d] \"%s\" (%d). Sending a reply...\n", *_msgFrom,
+                          msgRcv.c_str(), RFM95Modem_.lastRssi());
+        }
+        else
+        {
+            Serial.println("No reply, is the target node running?");
+        }
+
+        esp_task_wdt_reset();
+    }
+    else
+    {
+        Serial.println(
+            "sendtoWait failed. No response from intermediary node, are they "
+            "running?");
+
+        // TODO: If nobody received, up power? Maybe a function to do default power applied (starts at low value and increases, then stays at whatever value works)
+    }
+}
+
+void runGatewayReceiver(uint16_t wait_time, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_)
+{
+
+    Serial.println("Receiving mode active");
+    uint64_t start = millis();
+
+    // For wait_time, the gateway will wait for messages (multiple), and then post them to the backend
+    while (millis() - start < wait_time)
+    {
+        if (RHMeshManager_.recvfromAckTimeout(_msgRcvBuf, _msgRcvBufLen, gateway_wait, _msgFrom))
+        {
+            char buf_[RH_MESH_MAX_MESSAGE_LEN];
+
+            esp_task_wdt_reset();
+            Serial.println("Received a message");
+            std::sprintf(buf_, "%s", reinterpret_cast<char *>(_msgRcvBuf));
+            msgRcv = std::string(buf_);
+
+            // In here, we would want to take that message and do an HTTP POST to the backend with an auth key
+            // instead of just sending a reply
+            Serial.printf("[%d] \"%s\" (%d). Sending a reply...\n", _msgFrom,
+                          msgRcv.c_str(), RFM95Modem_.lastRssi());
+
+            msgRcv = "";
+
+            std::string _msgRply = String("Hi node " + String(*_msgFrom) + ", got the message!").c_str();
+            uint8_t _err = RHMeshManager_.sendtoWait(
+                reinterpret_cast<uint8_t *>(&_msgRply[0]), _msgRply.size(), *_msgFrom);
+            if (_err != RH_ROUTER_ERROR_NONE)
+            {
+                Serial.println("Fail to send reply...");
+            }
+            esp_task_wdt_reset();
+        }
+    }
+}
+
+void runGatewaySender(String *packetInfo, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_)
+{
+    uint8_t _err =
+        RHMeshManager_.sendtoWait(reinterpret_cast<uint8_t *>(packetInfo),
+                                  packetInfo->length(), 255);
+    if (_err == RH_ROUTER_ERROR_NONE)
+    {
+        // message successfully be sent to the target node, or next neighboring
+        // expecting to recieve a simple reply from the target node
+        esp_task_wdt_reset();
+        Serial.printf(" successfull! Awaiting for Reply\n");
+
+        // This eventually will need to be replaced
         if (RHMeshManager_.recvfromAckTimeout(_msgRcvBuf, _msgRcvBufLen, 3000, _msgFrom))
         {
             char buf_[RH_MESH_MAX_MESSAGE_LEN];

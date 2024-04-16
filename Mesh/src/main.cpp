16b0ba58266e7583ff4f7736ed38a5e21ef3563e
MadBeignet <maddie.wisinski@ymail.com>
2024-04-15 19:00:32 -0500

    unsure

---
+++ Mesh/src/main.cpp
@@ -0,0 +1,105 @@
+#include <RHMesh.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+#include <esp_task_wdt.h>
+#include <Wire.h>
+#include <cstring>
+#include "driver/gpio.h"
+#include "driver/pcnt.h"
+#include "ClosedCube_HDC1080.h"
+#include "esp_wifi.h"
+#include <driver/adc.h>
+#include "radio_pinouts_and_constants.h"
+#include "meshing.h"
+#include "measurement.h"
+#include <EEPROM.h>
+
+struct timeval tv_now;
+struct timeval start;
+
+void rhSetup();
+void runSending(String packetInfo, uint8_t targetAddress_, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_);
+void runReceiver(uint16_t time, uint8_t *_msgRcvBuf, uint8_t *_msgRcvBufLen, uint8_t *_msgFrom, RH_RF95 RFM95Modem_, RHMesh RHMeshManager_);
+
+void setup()
+{
+    gettimeofday(&start, NULL);
+    Serial.begin(115200);
+    esp_wifi_set_mode(WIFI_MODE_NULL);
+
+    pinMode(moisture, INPUT);
+    pinMode(light, INPUT);
+    pinMode(battery, INPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(misoPin, INPUT);
+    pinMode(mosiPin, OUTPUT);
+    pinMode(GPIO_NUM_26, OUTPUT);
+
+    esp_task_wdt_init(WDT_TIMEOUT, true); // enable panic so ESP32 restarts
+    esp_task_wdt_add(NULL);
+
+    adc2_config_channel_atten(ADC2_CHANNEL_8, ADC_ATTEN_0db);
+    // add current thread to WDT watch
+
+    EEPROM.begin(EEPROM_SIZE);
+    measureSetup();
+    rhSetup();
+    Serial.println(" ---------------- LORA NODE " + String(selfAddress_) +
+                   " INIT ---------------- ");
+}
+
+uint8_t _msgRcvBuf[RH_MESH_MAX_MESSAGE_LEN];
+
+void loop()
+{
+    // It wakes and starts here (always takes a measurement upon waking up)
+    Measurement m = getReadings();
+    m.printMeasurement();            // Prints measurement (this will not be needed later)
+    boolean isFull = saveReading(m); // save the reading to flash (also gets a boolean if the readings are full)
+
+    // change to sending mode if readings are full AND this is not the endnode
+    mode_ = (isFull && selfAddress_ != ENDNODE_ADDRESS) ? SENDING_MODE : SENSING_MODE;
+
+    if (mode_ == SENSING_MODE)
+    {
+        Serial.println("Start: " + String(start.tv_sec) + "." + String(start.tv_usec));
+        gettimeofday(&tv_now, NULL); // get time of day
+        // Calculates time it takes between startup and now
+        uint64_t sleepTime = ((timer + start.tv_sec - tv_now.tv_sec)) * microseconds + start.tv_usec - tv_now.tv_usec;
+        Serial.println("Sleeping at: " + String(tv_now.tv_sec) + "." + String(tv_now.tv_usec) + " seconds and for: " + String((double)sleepTime / microseconds) + " seconds");
+        esp_err_t sleep_error = esp_sleep_enable_timer_wakeup(sleepTime); // takes into account time between start and sleep
+        esp_deep_sleep_start();
+    }
+
+    printReadings(); // unnecessary later
+
+    uint8_t _msgFrom;
+    uint8_t _msgRcvBufLen = sizeof(_msgRcvBuf);
+
+    if (mode_ == SENDING_MODE)
+    {
+        // Send a message to another rhmesh node
+        // TODO: Send our data here
+        String packetInfo = "Hello"; // temp message
+        Serial.printf("Sending data to %d...", targetAddress_);
+        runSending(&packetInfo, targetAddress_, _msgRcvBuf, &_msgRcvBufLen, &_msgFrom, RFM95Modem_, RHMeshManager_);
+
+        // Prepare for new readings (would be next day)
+        isFull = false;
+        clearReadings();
+
+        mode_ = RECEIVING_MODE;
+    }
+
+    if (mode_ == RECEIVING_MODE)
+    {
+        // We need to be receiving for a random time
+        uint16_t wait_time = random(1000, 5000);
+        runReceiver(wait_time, _msgRcvBuf, &_msgRcvBufLen, &_msgFrom, RFM95Modem_, RHMeshManager_);
+    }
+    esp_task_wdt_reset();
+
+    Serial.println("Sleeping: " + String(tv_now.tv_sec) + "." + String(tv_now.tv_usec) + " seconds");
+    esp_err_t sleep_error = esp_sleep_enable_timer_wakeup(timer * microseconds);
+    esp_deep_sleep_start();
+}
---
+++ Mesh/src/radio_pinouts_and_constants.h
@@ -0,0 +1,63 @@
+#include <RH_RF95.h>
+
+// Flash Memory Allocation
+#define EEPROM_SIZE 120 // 5 readings * 8 bytes (double) * 3 packets = 120 bytes
+
+// For Device Sleep
+uint64_t time_period = 40;                       // 40 seconds
+#define num_measurements 4                       // num measurements to take in a set time
+uint64_t timer = time_period / num_measurements; // (equally spaces out measurements) converted to microseconds in code
+#define microseconds 1000000                     // 1 second in microseconds
+#define hours_to_seconds 3600                    // 1 hour in seconds
+
+// Radio Constants
+#define RF95_FREQ 915.0 // USA and Ecuador
+#define WDT_TIMEOUT 15
+
+#if defined(RFM95_CS) && defined(RFM95_RST) && defined(RFM95_INT)
+#else
+
+// Board pinout (from mesh)
+#define RFM95_CS 5
+#define RFM95_RST 14
+#define RFM95_INT 13
+#endif
+
+#define SENDING_MODE 0
+#define RECEIVING_MODE 1
+#define SENSING_MODE 2
+
+#if defined(SELF_ADDRESS) && defined(TARGET_ADDRESS)
+const uint8_t selfAddress_ = SELF_ADDRESS;
+const uint8_t targetAddress_ = TARGET_ADDRESS;
+#else
+// Topology
+// define the node address
+#define NODE_ADDRESS 3
+#define ENDNODE_ADDRESS 255 // purposefully using the last number
+// TODO: according to this, we might have a max of 256 nodes in one mesh
+// selfAddress is node
+// targetAddress will be our gateway
+const uint8_t selfAddress_ = NODE_ADDRESS;      // CHANGE THIS!!!
+const uint8_t targetAddress_ = ENDNODE_ADDRESS; // integer value
+#endif
+
+// radio driver & message mesh delivery/receipt manager
+RH_RF95 RFM95Modem_(RFM95_CS, RFM95_INT);
+RHMesh RHMeshManager_(RFM95Modem_, selfAddress_);
+uint8_t mode_ = SENSING_MODE;
+
+// these are expected to be global/externally exposed variables, if you plan to
+// make a class to wrap this
+std::string msgSend =
+    String("Hello from node " + String(selfAddress_) + "!").c_str();
+std::string msgRcv;
+
+void rhSetup()
+{
+    if (!RHMeshManager_.init())
+        Serial.println("init failed");
+    RFM95Modem_.setTxPower(17, false);
+    RFM95Modem_.setFrequency(RF95_FREQ);
+    RFM95Modem_.setCADTimeout(500);
+}

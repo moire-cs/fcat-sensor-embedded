313eee220884a9cd24db13faf4a6e311ec9476ad
jJokerjokes <hzhang32@tulane.edu>
2025-02-16 16:16:58 -0600

    time counting from float to int

--- src/radio_pinouts_and_constants.h
+++ src/radio_pinouts_and_constants.h
@@ -48,15 +48,18 @@ void printPacket(struct Packet p) {
 RTC_DATA_ATTR struct Measurement measurements[MAX_MEASUREMENTS]; // measurements stored in RTC memory
 
 // For Device Sleep
-RTC_DATA_ATTR float duration = 0.02;                              // hours
-RTC_DATA_ATTR unsigned int num_measurements = 2;                             // num measurements to take in a set time
+RTC_DATA_ATTR unsigned int hour = 0;               // hours
+RTC_DATA_ATTR unsigned int min = 1;               //minutes
+RTC_DATA_ATTR unsigned int sec = 12;              //seconds  
+RTC_DATA_ATTR unsigned int num_measurements = 2;                // num measurements to take in a set time
 RTC_DATA_ATTR float time_sync_tolerance = 0.005; // factor
 RTC_DATA_ATTR float mesh_sync_tolerance = 0.005; // factor
 
-#define microseconds 1000000                           // 1 second in microseconds
+#define microseconds 1000                           // 1 second in microseconds
+#define minutes_to_seconds 60
 #define hours_to_seconds 3600
 
-RTC_DATA_ATTR uint64_t timer = duration * hours_to_seconds / (num_measurements); // (equally spaces out measurements) converted to microseconds in code
+RTC_DATA_ATTR uint64_t timer = (hour * hours_to_seconds + min * minutes_to_seconds + sec)*microseconds/(num_measurements); // (equally spaces out measurements) converted to microseconds in code
 
 
 
